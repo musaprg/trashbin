0515805ee5481003e0af2aacd166b79c886f2602
Kotaro <k.musaino@gmail.com>
2019-08-25 15:17:28 +0900

    [flutter] Add Likes Icon

--- flutter/startup_namer/lib/main.dart
+++ flutter/startup_namer/lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
@@ -68,11 +69,16 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      )
     );
   }
 }
